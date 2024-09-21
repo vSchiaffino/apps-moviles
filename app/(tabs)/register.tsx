d89737215af931de161f1eeded73a7b5ef6d5b2a
Jorge_N <valentinnave@gmail.com>
2024-09-21 11:22:45 -0300

    add register component and tab

---
+++ app/(tabs)/register.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Register from '@/components/Register'
+
+const register = () => {
+  return (
+    <>
+      <Register/>
+    </>
+  )
+}
+
+export default register
