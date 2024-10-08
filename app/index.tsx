4db2c946aa56e1e66539c6a4a9847cb7da8dc840
Jorge <valentinnave@gmail.com>
2024-10-08 09:10:46 -0300

    bugfix: welcome message now returns the correct react-native components

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import useUser from '@/hooks/useUser'
 import { Redirect } from 'expo-router'
 import React from 'react'
+import { Text, View }  from 'react-native'
 
 const Dashboard = () => {
   const { user } = useUser()
   if (!user) return <Redirect href='/login' />
-  return <div>Hola {user.user}</div>
+  return <View><Text>Hola {user.user}</Text></View>
 }
 
 export default Dashboard
