ebb13a77751bce842aa3a0f3a3f538fab6e8062f
Jorge <valentinnave@gmail.com>
2024-10-13 23:47:02 -0300

    Modal styling and functionality

--- pages/ProfilePage.tsx
+++ pages/ProfilePage.tsx
@@ -7,9 +7,10 @@ import StyledButton from '@/components/StyledButton'
 import { Colors } from '../constants/Colors'
 import Container from '@/components/Container'
 import ValidatedForm, { ValidatedField } from '@/components/ValidatedForm'
-import { ScrollView } from 'react-native-gesture-handler'
+import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler'
 import Modal from 'react-native-modal'
 import * as ImagePicker from 'expo-image-picker'
+import IconCard from '@/components/IconCard'
 
 interface ProfilePageProps {
   user: UserPayload
@@ -21,6 +22,31 @@ const ProfilePage = ({ user }: ProfilePageProps) => {
 
   const onSubmit = () => {
     //TODO: Handle onSubmit change user
+  }
+
+  const uploadImage = async () => {
+    try {
+      await ImagePicker.getCameraPermissionsAsync()
+      let result = await ImagePicker.launchCameraAsync({
+        cameraType: ImagePicker.CameraType.front,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+      })
+      if (!result.canceled) {
+        await saveImage(result.assets[0].uri)
+      }
+    } catch (error: any) {
+      alert('Error uploading image: ' + error.message)
+      setModalVisible(false)
+    }
+  }
+
+  const saveImage = async (image: string) => {
+    try {
+      setUserPic(image)
+      setModalVisible(false)
+    } catch (error) {}
   }
 
   const fields: ValidatedField[] = [
@@ -98,132 +124,140 @@ const ProfilePage = ({ user }: ProfilePageProps) => {
   ]
 
   return (
-    <ScrollView>
-      <Container
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Modal
-          isVisible={modalVisible}
-          animationIn="fadeIn"
-          animationInTiming={300}
-          animationOut="fadeOut"
-          animationOutTiming={300}
-          hideModalContentWhileAnimating={true}
-          backdropOpacity={0.3}
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ScrollView>
+        <Container
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
         >
+          <Modal
+            isVisible={modalVisible}
+            animationIn="fadeIn"
+            animationInTiming={300}
+            animationOut="fadeOut"
+            animationOutTiming={300}
+            hideModalContentWhileAnimating={true}
+            backdropOpacity={0.3}
+            onBackButtonPress={() => setModalVisible(false)}
+          >
+            <View
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                height: '25%', //Hardcoded, should be fit-content like property
+              }}
+            >
+              <View
+                style={{
+                  gap: 10,
+                  width: '100%',
+                  backgroundColor: 'white',
+                  padding: 20,
+                  borderRadius: 25,
+                }}
+              >
+                <View
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                  }}
+                >
+                  <Typography variant="h5">Elegí una foto</Typography>
+                  <Pressable onPress={() => setModalVisible(false)}>
+                    <MaterialIcons name="close" size={24}></MaterialIcons>
+                  </Pressable>
+                </View>
+                <View
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    height: '100%',
+                    width: '100%',
+                    justifyContent: 'center',
+                    gap: 10,
+                  }}
+                >
+                  <IconCard icon="camera-outline" color={'gray'} text="Camera" />
+                  <IconCard icon="image-outline" color={'gray'} text="Gallery" />
+                  <IconCard icon="trash-outline" color={'danger'} text="Remove" />
+                </View>
+              </View>
+            </View>
+          </Modal>
+
           <View
             style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              width: '100%',
-              height: '100%',
+              backgroundColor: Colors.primary[600],
+              width: 200,
+              height: 200,
+              marginTop: 250,
+              padding: 7,
+              borderRadius: 999,
             }}
           >
             <View
               style={{
-                gap: 10,
-                width: '80%',
-                height: '50%',
-                backgroundColor: 'white',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                position: 'absolute',
+                zIndex: 100,
+                left: '70%',
+                backgroundColor: Colors.primary[600],
+                borderRadius: 999,
                 padding: 20,
-                borderRadius: 25,
+                width: 25,
+                height: 25,
               }}
             >
-              <Typography variant="h5">Elegí una foto</Typography>
-              <View
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  width: '100%',
-                  justifyContent: 'center',
-                  gap: 10,
-                }}
-              >
-                <StyledButton
-                  label="Cancelar"
-                  style={{ backgroundColor: Colors.danger[600], width: '50%' }}
-                  onPress={() => setModalVisible(false)}
-                />
-                <StyledButton
-                  label="Guardar"
-                  style={{ backgroundColor: Colors.primary[600], width: '50%' }}
-                  onPress={() => setModalVisible(false)}
-                />
-              </View>
+              <Pressable onPress={() => setModalVisible(true)} style={{ position: 'absolute' }}>
+                <MaterialIcons name="edit" size={25} color={'white'} />
+              </Pressable>
             </View>
+            <Image
+              source={{ uri: userPic }}
+              style={{
+                resizeMode: 'cover',
+                width: '100%',
+                height: '100%',
+                borderRadius: 999,
+              }}
+            />
           </View>
-        </Modal>
 
-        <View
-          style={{
-            backgroundColor: Colors.primary[600],
-            width: 200,
-            height: 200,
-            marginTop: 250,
-            padding: 7,
-            borderRadius: '50%',
-          }}
-        >
-          <View
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              position: 'absolute',
-              zIndex: 100,
-              left: '70%',
-              backgroundColor: Colors.primary[600],
-              borderRadius: '50%',
-              padding: 20,
-              width: 25,
-              height: 25,
-            }}
-          >
-            <Pressable onPress={() => setModalVisible(true)} style={{ position: 'absolute' }}>
-              <MaterialIcons name="edit" size={25} color={'white'} />
-            </Pressable>
-          </View>
-          <Image
-            source={{ uri: userPic }}
-            style={{
-              resizeMode: 'cover',
-              width: '100%',
-              height: '100%',
-              borderRadius: '50%',
-            }}
-          />
-        </View>
+          <Typography variant="h4">{user.user}</Typography>
 
-        <Typography variant="h4">{user.user}</Typography>
-
-        <Container style={{ gap: 15, marginTop: 0 }}>
-          <Typography variant="h6" color="gray">
-            Datos Personales
-          </Typography>
-          <ValidatedForm
-            formProps={{
-              defaultValues: {
-                user: user.user,
-                name: user.name,
-                lastName: user.lastName,
-                mail: user.mail,
-                actualPassword: '',
-                newPassword: '',
-                repeatPassword: '',
-              },
-            }}
-            submitLabel="Guardar"
-            onSubmit={() => onSubmit()}
-            fields={fields}
-          />
+          <Container style={{ gap: 15, marginTop: 0 }}>
+            <Typography variant="h6" color="gray">
+              Datos Personales
+            </Typography>
+            <ValidatedForm
+              formProps={{
+                defaultValues: {
+                  user: user.user,
+                  name: user.name,
+                  lastName: user.lastName,
+                  mail: user.mail,
+                  actualPassword: '',
+                  newPassword: '',
+                  repeatPassword: '',
+                },
+              }}
+              submitLabel="Guardar"
+              onSubmit={() => onSubmit()}
+              fields={fields}
+            />
+          </Container>
         </Container>
-      </Container>
-    </ScrollView>
+      </ScrollView>
+    </GestureHandlerRootView>
   )
 }
 
