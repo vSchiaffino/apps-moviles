83175dcf717c9c16b65200b3afa787e3636d3e45
Valentin Schiaffino <valenschg@gmail.com>
2024-10-14 19:37:03 -0300

    fix: delete commited change

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,25 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import useUser from '@/hooks/useUser'
 import { Redirect } from 'expo-router'
 import ProfilePage from '@/pages/ProfilePage/ProfilePage'
 
 const profile = () => {
-  // const { user } = useUser()
-  // if (!user) return <Redirect href="/" />
-  return (
-    <ProfilePage
-      user={{
-        id: 1,
-        name: 'Valentin',
-        lastName: 'Schiaffino',
-        mail: 'valenschg@gmail.com',
-        user: 'valen',
-      }}
-    />
-  )
+  const { user } = useUser()
+  if (!user) return <Redirect href="/" />
+  return <ProfilePage user={user} />
 }
 
 export default profile
-
-const styles = StyleSheet.create({})
