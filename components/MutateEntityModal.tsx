5acc57e0b6ad37b8bc88ff0dcf1c497991805f82
Valentin Schiaffino <vschiaffino@urbano.com.ar>
2024-11-09 20:34:32 -0300

    refactor: extract MutateEntityModal

---
+++ components/MutateEntityModal.tsx
@@ -0,0 +1,81 @@
+import { View, Pressable } from 'react-native'
+import React from 'react'
+import ReactNativeModal from 'react-native-modal'
+import Typography from '@/components/Typography'
+import Card from '@/components/Card'
+import { Ionicons } from '@expo/vector-icons'
+import { Colors } from '@/constants/Colors'
+
+export interface MutateEntityModalProps {
+  show: boolean
+  setShow: Function
+  isCreating: boolean
+  entityName: string
+  children: React.ReactNode
+}
+
+const MutateEntityModal: React.FC<MutateEntityModalProps> = ({
+  show,
+  setShow,
+  isCreating,
+  entityName,
+  children,
+}) => {
+  const action = isCreating ? 'Creando' : 'Editando'
+  return (
+    <ReactNativeModal
+      isVisible={show}
+      animationIn="slideInUp"
+      animationOut="slideOutDown"
+      animationInTiming={300}
+      animationOutTiming={300}
+      hideModalContentWhileAnimating={true}
+      useNativeDriver={true}
+      onBackButtonPress={() => setShow(false)}
+    >
+      <Pressable
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+        }}
+        onPress={() => setShow(false)}
+      />
+      <Card
+        style={{
+          height: 'auto',
+          maxHeight: '80%',
+          padding: 0,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            padding: 20,
+            borderTopLeftRadius: 16,
+            borderTopRightRadius: 16,
+            backgroundColor: Colors.gray[100],
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h5">{action} un {entityName}</Typography>
+          <Ionicons
+            name="close"
+            size={30}
+            style={{
+              marginLeft: 'auto',
+              backgroundColor: Colors.gray[200],
+              borderRadius: 999,
+              padding: 5,
+            }}
+            color={Colors.gray[800]}
+            onPress={() => setShow(false)}
+          />
+        </View>
+        {children}
+      </Card>
+    </ReactNativeModal>
+  )
+}
+
+export default MutateEntityModal
--- pages/WarehousePage/WarehouseModal.tsx
+++ pages/WarehousePage/WarehouseModal.tsx
@@ -1,20 +1,14 @@
-import { View, Pressable } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
-import ReactNativeModal from 'react-native-modal'
-import Typography from '@/components/Typography'
-import Card from '@/components/Card'
-import { Ionicons } from '@expo/vector-icons'
-import { Colors } from '@/constants/Colors'
 import ValidatedForm, { ValidatedField } from '@/components/ValidatedForm'
+import MutateEntityModal, { MutateEntityModalProps } from '@/components/MutateEntityModal'
+import { Spacing } from '@/constants/Spacing'
 
-export interface WarehouseModalProps {
-  show: boolean
-  setShow: Function
+export interface WarehouseModalProps extends MutateEntityModalProps {
   onSubmit: (form: any) => Promise<void>
 }
 
-// TODO: this modal could be extracted as a generic create/edit entity
-const WarehouseModal: React.FC<WarehouseModalProps> = ({ show, setShow, onSubmit }) => {
+const WarehouseModal: React.FC<WarehouseModalProps> = ({ onSubmit, ...rest }) => {
   const fields: ValidatedField[] = [
     {
       name: 'name',
@@ -46,70 +40,21 @@ const WarehouseModal: React.FC<WarehouseModalProps> = ({ show, setShow, onSubmit
     },
   ]
   return (
-    <ReactNativeModal
-      isVisible={show}
-      animationIn="slideInUp"
-      animationOut="slideOutDown"
-      animationInTiming={300}
-      animationOutTiming={300}
-      hideModalContentWhileAnimating={true}
-      useNativeDriver={true}
-      onBackButtonPress={() => setShow(false)}
-    >
-      <Pressable
-        style={{
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-        }}
-        onPress={() => setShow(false)}
-      />
-      <Card
-        style={{
-          height: 'auto',
-          maxHeight: '80%',
-          padding: 0,
-        }}
-      >
-        <View
-          style={{
-            flexDirection: 'row',
-            padding: 20,
-            borderTopLeftRadius: 16,
-            borderTopRightRadius: 16,
-            backgroundColor: Colors.gray[100],
-            alignItems: 'center',
+    <MutateEntityModal {...rest}>
+      <View style={{ flexDirection: 'column', gap: Spacing.rowGap, padding: 20 }}>
+        <ValidatedForm
+          fields={fields}
+          onSubmit={onSubmit}
+          submitLabel="Crear"
+          formProps={{
+            defaultValues: {
+              name: '',
+              capacity: '',
+            },
           }}
-        >
-          <Typography variant="h5">Creando un depósito</Typography>
-          <Ionicons
-            name="close"
-            size={30}
-            style={{
-              marginLeft: 'auto',
-              backgroundColor: Colors.gray[200],
-              borderRadius: 999,
-              padding: 5,
-            }}
-            color={Colors.gray[800]}
-            onPress={() => setShow(false)}
-          />
-        </View>
-        <View style={{ flexDirection: 'column', gap: 20, padding: 20 }}>
-          <ValidatedForm
-            fields={fields}
-            onSubmit={onSubmit}
-            submitLabel="Crear"
-            formProps={{
-              defaultValues: {
-                name: '',
-                capacity: '',
-              },
-            }}
-          />
-        </View>
-      </Card>
-    </ReactNativeModal>
+        />
+      </View>
+    </MutateEntityModal>
   )
 }
 
