8ac7fd8f39a61bcffa080e61306230ad0904ae90
Jorge <valentinnave@gmail.com>
2024-11-10 16:08:52 -0300

    hide pageHeader if not defined

--- components/Container.tsx
+++ components/Container.tsx
@@ -14,36 +14,40 @@ export interface ContainerProps {
 const Container: React.FC<ContainerProps> = ({ children, style, pageHeader }) => {
   return (
     <>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          shadowColor: 'black',
-          shadowRadius: 5,
-          shadowOffset: { width: 10, height: 10 },
-          elevation: 3,
-          shadowOpacity: 1,
-          backgroundColor: Colors.primary[100],
-          paddingTop: 52,
-          padding: 10,
-          zIndex: 999,
-        }}
-      >
-        <Ionicons
-          name="chevron-back-sharp"
-          size={30}
-          color={Colors.gray[700]}
-          style={{ paddingBottom: 5, position: 'absolute', right: '93%', bottom: '33%' }}
-        />
-        <Typography variant="h4" style={{ color: Colors.gray[700] }}>
-          {pageHeader}
-        </Typography>
-      </View>
+      {pageHeader !== undefined && (
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            shadowColor: 'black',
+            shadowRadius: 5,
+            shadowOffset: { width: 10, height: 10 },
+            elevation: 3,
+            shadowOpacity: 1,
+            backgroundColor: Colors.primary[100],
+            paddingTop: 52,
+            padding: 10,
+            zIndex: 999,
+          }}
+        >
+          <Ionicons
+            name="chevron-back-sharp"
+            size={30}
+            color={Colors.gray[700]}
+            style={{ paddingBottom: 5, position: 'absolute', right: '93%', bottom: '33%' }}
+          />
+          <Typography variant="h4" style={{ color: Colors.gray[700] }}>
+            {pageHeader}
+          </Typography>
+        </View>
+      )}
+
       <View
         style={{
           width: '100%',
           height: '100%',
+          paddingTop: pageHeader === undefined ? 48 : 16,
           padding: 16,
           backgroundColor: Colors.gray[100],
           ...Object(style),
