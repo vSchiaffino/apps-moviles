cde02cc59e15297539f70964e0418131001a98d0
Yr3b <102194010+Yr3b@users.noreply.github.com>
2024-11-11 02:08:20 -0300

    Add SM Page to app

---
+++ app/stockManager.tsx
@@ -0,0 +1,7 @@
+import { useAuthorizedUser } from '@/hooks/useUser'
+import StockManagerPage from '@/pages/StockPage/StockManagerPage'
+
+export default function () {
+  //useAuthorizedUser()
+  return <StockManagerPage />
+}
