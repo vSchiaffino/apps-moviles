aa8f2a5b067950dc125c9e4c4d7f5e189d53007a
Jorge <valentinnave@gmail.com>
2024-11-26 18:01:31 -0300

    shift page

--- pages/StartShiftPage/StartShiftPage.tsx
+++ pages/StartShiftPage/StartShiftPage.tsx
@@ -1,11 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, { useState } from 'react'
 import Container from '@/components/Container'
+import InfoCard from '@/components/InfoCard'
+import { Spacing } from '@/constants/Spacing'
+import StyledButton from '@/components/StyledButton'
+import { router } from 'expo-router'
 
 const StartShiftPage = () => {
+  const [selectedWarehouse, setSelectedWarehouse] = useState<string>('')
   return (
     <Container>
-      <Text>StartShiftPage</Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{ padding: 16, height: '100%' }}
+        contentContainerStyle={{ gap: Spacing.rowGap }}
+      >
+        {selectedWarehouse === '' && (
+          <>
+            <InfoCard infoText="Seleccioná un depósito para comenzar el turno" />
+            <StyledButton
+              label="Seleccionar Depósito"
+              onPress={() => {
+                router.push('/profile')
+                setSelectedWarehouse('hola')
+              }}
+            />
+          </>
+        )}
+      </ScrollView>
     </Container>
   )
 }
