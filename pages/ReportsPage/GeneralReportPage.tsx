ee73ee242c521f14ad6f51fc9a34afb23cc0dffe
Gonzalo <gonzasoler.20@gmail.com>
2024-11-29 18:04:32 -0300

    Extract report chart component

--- pages/ReportsPage/GeneralReportPage.tsx
+++ pages/ReportsPage/GeneralReportPage.tsx
@@ -1,142 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import { View, ScrollView, StyleSheet, Dimensions } from 'react-native'
-import { BarChart } from 'react-native-gifted-charts'
-import Typography from '@/components/Typography'
-import DateSelect from '@/components/DateSelect'
-import SelectDateModal from '@/pages/StockPage/SelectDateModal'
-import StyledButton from '@/components/StyledButton'
-import { useReportData } from '@/hooks/useReportData'
-import InfoCard from '@/components/InfoCard'
-import useOneShiftData from '@/hooks/useOneShiftData'
+import React from 'react';
+import ReportComponent from '@/components/ReportChart';
+import { useReportData } from '@/hooks/useReportData';
 
+type ChartData = {
+    label: string; 
+    value: number; 
+    id?: string | number; 
+  };
+  
 const GeneralReport = () => {
-  const [selectedInitialDate, setSelectedInitialDate] = useState('')
-  const [selectedFinalDate, setSelectedFinalDate] = useState('')
-  const [initialModalVisible, setInitialModalVisible] = useState(false)
-  const [finalModalVisible, setFinalModalVisible] = useState(false)
-  const [showReport, setShowReport] = useState(false)
-  const { report } = useReportData(selectedInitialDate, selectedFinalDate)
-  const sum = (array: number[]) => array.reduce((a, b) => a + b, 0)
+  const calculateData = (report: any): ChartData[] => {
+    if (!report) return [];
 
-const data = report
-    ?.map((report) => ({
-      label: new Date(report.startDate)
-        .toLocaleString('es-ES', {
-          day: '2-digit',
-          month: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-          hour12: false,
-        })
-        .replace(',', ''),
-      value: sum(report.missing.map((missing) => sum(missing.stock.map((s) => s.quantity)))),
-      id: report.id,
-    }))
-    .filter((r) => r.value > 0)
+    const sum = (array: number[]) => array.reduce((a, b) => a + b, 0);
 
-  const [id, setId] = useState<number | undefined>(undefined)
-  const { shift } = useOneShiftData(id)
-  console.log('shift', shift)
+    return report
+      ?.map((item: { startDate: string | number | Date; missing: any[]; id: any; }) => ({
+        label: new Date(item.startDate)
+          .toLocaleString('es-ES', {
+            day: '2-digit',
+            month: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false,
+          })
+          .replace(',', ''),
+        value: sum(item.missing.map((m: { stock: any[]; }) => sum(m.stock.map((s: { quantity: any; }) => s.quantity)))),
+        id: item.id,
+      }))
+      .filter((r: { value: number; }) => r.value > 0);
+  };
 
   return (
-    <ScrollView style={{ height: '100%' }}>
-      <View style={{ padding: 16 }}>
-        <DateSelect
-          label="Fecha inicio"
-          value={selectedInitialDate}
-          onChange={setSelectedInitialDate}
-          onPressIcon={() => setInitialModalVisible(true)}
-        />
-        <SelectDateModal
-          selectedDate={selectedInitialDate}
-          setSelectedDate={setSelectedInitialDate}
-          visible={initialModalVisible}
-          setVisible={setInitialModalVisible}
-        />
+    <ReportComponent
+      useReportHook={useReportData} 
+      calculateData={calculateData}
+      subtitle="Comparación de Ventas y Disminución de Stock"
+      xAxisLabel="Días"
+      yAxisLabel="Unidades por pérdida"
+    />
+  );
+};
 
-        <DateSelect
-          label="Fecha final"
-          value={selectedFinalDate}
-          onChange={setSelectedFinalDate}
-          onPressIcon={() => setFinalModalVisible(true)}
-        />
-        <SelectDateModal
-          selectedDate={selectedFinalDate}
-          setSelectedDate={setSelectedFinalDate}
-          visible={finalModalVisible}
-          setVisible={setFinalModalVisible}
-        />
-        <View style={{ marginBottom: '5%' }}>
-          <StyledButton label="Ver reportes" onPress={() => setShowReport(true)} />
-        </View>
-        {showReport && selectedInitialDate !== '' && selectedFinalDate !== '' ? (
-          <View style={styles.chartContainer}>
-            <Typography variant="subtitle" style={[styles.centeredText, { marginTop: 24 }]}>
-              Comparación de Ventas y Disminución de Stock
-            </Typography>
-            <Typography variant="mini" style={styles.yAxisLabel}>
-              Unidades por pérdida
-            </Typography>
-            <ScrollView style={styles.chartWrapper} horizontal={true}>
-              {data && (
-                <BarChart
-                  data={data}
-                  barWidth={40}
-                  xAxisLabelTextStyle={{ fontSize: 10 }}
-                  barBorderRadius={5}
-                  frontColor="#4CAF50"
-                  showXAxisIndices={true}
-                  onPress={(value: any) => console.log(value)}
-                />
-              )}
-              <Typography variant="body" style={styles.xAxisLabel}>
-                Días
-              </Typography>
-            </ScrollView>
-          </View>
-        ) : (
-          <InfoCard infoText="Seleccionar un rango de fechas para ver los reportes" />
-        )}
-      </View>
-    </ScrollView>
-  )
-}
-
-const styles = StyleSheet.create({
-  centeredText: {
-    textAlign: 'center',
-  },
-  chartContainer: {
-    marginTop: 20,
-  },
-  yAxisLabel: {
-    fontSize: 14,
-    color: '#000',
-    fontWeight: 'bold',
-  },
-  chartWrapper: {
-    //alignItems: 'center',
-    marginVertical: 20,
-  },
-  xAxisLabel: {
-    position: 'absolute',
-    bottom: -20,
-    fontSize: 14,
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    borderRadius: 10,
-    padding: 20,
-    width: 300,
-    height: 350,
-    alignItems: 'center',
-  },
-  closeButton: {
-    backgroundColor: '#3498db',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 10,
-  },
-})
-
-export default GeneralReport
+export default GeneralReport;
--- pages/ReportsPage/ProductReportPage.tsx
+++ pages/ReportsPage/ProductReportPage.tsx
@@ -1,153 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import { View, ScrollView, StyleSheet, Dimensions } from 'react-native'
-import { BarChart } from 'react-native-gifted-charts'
-import Typography from '@/components/Typography'
-import DateSelect from '@/components/DateSelect'
-import SelectDateModal from '@/pages/StockPage/SelectDateModal'
-import StyledButton from '@/components/StyledButton'
-import { useReportData } from '@/hooks/useReportData'
-import InfoCard from '@/components/InfoCard'
-import useOneShiftData from '@/hooks/useOneShiftData'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import ReportComponent from '@/components/ReportChart';
+import { useReportData } from '@/hooks/useReportData';
 
-const ProductReport = () => {
-  const [selectedInitialDate, setSelectedInitialDate] = useState('')
-  const [selectedFinalDate, setSelectedFinalDate] = useState('')
-  const [initialModalVisible, setInitialModalVisible] = useState(false)
-  const [finalModalVisible, setFinalModalVisible] = useState(false)
-  const [showReport, setShowReport] = useState(false)
-  const { report } = useReportData(selectedInitialDate, selectedFinalDate)
-  const sum = (array: number[]) => array.reduce((a, b) => a + b, 0)
+const ProductReport: React.FC = () => {
+  const useReportHook = useReportData;
 
-  const products = Array.from(
-    new Map(
-      report
-        ?.flatMap((r) =>
-          r.missing.flatMap((m) =>
-            m.stock.map((s) => ({ id: s.productId, name: s.product?.name })),
-          ),
-        )
-        .filter((product) => product.name !== undefined)
-        .map((product) => [product.id, product]),
-    ).values(),
-  )
-  const data = products.map(({ id, name }) => ({
-    label: name,
-    value: sum(
-      report
-        ?.flatMap((r) =>
-          r.missing.flatMap((m) =>
-            m.stock.filter((s) => s.productId === id).map((s) => s.quantity),
-          ),
-        )
-        .filter((q) => q > 0),
-    ),
-  }))
+  const calculateData = (report: any) => {
+    const sum = (array: number[]) => array.reduce((a, b) => a + b, 0);
 
-  // gon este state es un ejemplo nomas de como podes usar el hook useOneShiftData para mostrar
-  // despues el detalle de un shift en particular cuando tocan en el grafico
-  const [id, setId] = useState<number | undefined>(undefined)
-  const { shift } = useOneShiftData(id)
-  console.log('shift', shift)
+    const products = Array.from(
+      new Map(
+        report
+          ?.flatMap((r: any) =>
+            r.missing.flatMap((m: any) =>
+              m.stock.map((s: any) => ({ id: s.productId, name: s.product?.name })),
+            ),
+          )
+          .filter((product: any) => product.name !== undefined)
+          .map((product: any) => [product.id, product]),
+      ).values(),
+    ) as { id: string; name: string }[];
+
+    return products.map(({ id, name }) => ({
+      label: name,
+      value: sum(
+        report
+          ?.flatMap((r: any) =>
+            r.missing.flatMap((m: any) =>
+              m.stock.filter((s: any) => s.productId === id).map((s: any) => s.quantity),
+            ),
+          )
+          .filter((q: number) => q > 0),
+      ),
+      id,
+    }));
+  };
 
   return (
-    <ScrollView style={{ height: '100%' }}>
-      <View style={{ padding: 16 }}>
-        <DateSelect
-          label="Fecha inicio"
-          value={selectedInitialDate}
-          onChange={setSelectedInitialDate}
-          onPressIcon={() => setInitialModalVisible(true)}
-        />
-        <SelectDateModal
-          selectedDate={selectedInitialDate}
-          setSelectedDate={setSelectedInitialDate}
-          visible={initialModalVisible}
-          setVisible={setInitialModalVisible}
-        />
+    <ReportComponent
+      useReportHook={useReportHook}
+      calculateData={calculateData}
+      subtitle="Comparación de Ventas y Disminución de Stock"
+      xAxisLabel="Días"
+      yAxisLabel="Unidades por pérdida"
+    />
+  );
+};
 
-        <DateSelect
-          label="Fecha final"
-          value={selectedFinalDate}
-          onChange={setSelectedFinalDate}
-          onPressIcon={() => setFinalModalVisible(true)}
-        />
-        <SelectDateModal
-          selectedDate={selectedFinalDate}
-          setSelectedDate={setSelectedFinalDate}
-          visible={finalModalVisible}
-          setVisible={setFinalModalVisible}
-        />
-        <View style={{ marginBottom: '5%' }}>
-          <StyledButton label="Ver reportes" onPress={() => setShowReport(true)} />
-        </View>
-        {showReport && selectedInitialDate !== '' && selectedFinalDate !== '' ? (
-          <View style={styles.chartContainer}>
-            <Typography variant="subtitle" style={[styles.centeredText, { marginTop: 24 }]}>
-              Comparación de Ventas y Disminución de Stock
-            </Typography>
-            <Typography variant="mini" style={styles.yAxisLabel}>
-              Unidades por pérdida
-            </Typography>
-            <ScrollView style={styles.chartWrapper} horizontal={true}>
-              {data && (
-                <BarChart
-                  data={data}
-                  barWidth={40}
-                  xAxisLabelTextStyle={{ fontSize: 10 }}
-                  barBorderRadius={5}
-                  frontColor="#4CAF50"
-                  showXAxisIndices={true}
-                  onPress={(value: any) => console.log(value)}
-                />
-              )}
-              <Typography variant="body" style={styles.xAxisLabel}>
-                Días
-              </Typography>
-            </ScrollView>
-          </View>
-        ) : (
-          <InfoCard infoText="Seleccionar un rango de fechas para ver los reportes" />
-        )}
-      </View>
-    </ScrollView>
-  )
-}
+export default ProductReport;
 
-const styles = StyleSheet.create({
-  centeredText: {
-    textAlign: 'center',
-  },
-  chartContainer: {
-    marginTop: 20,
-  },
-  yAxisLabel: {
-    fontSize: 14,
-    color: '#000',
-    fontWeight: 'bold',
-  },
-  chartWrapper: {
-    //alignItems: 'center',
-    marginVertical: 20,
-  },
-  xAxisLabel: {
-    position: 'absolute',
-    bottom: -20,
-    fontSize: 14,
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    borderRadius: 10,
-    padding: 20,
-    width: 300,
-    height: 350,
-    alignItems: 'center',
-  },
-  closeButton: {
-    backgroundColor: '#3498db',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 10,
-  },
-})
-
-export default ProductReport
