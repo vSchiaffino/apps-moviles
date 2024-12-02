b49c0dc673be9f2d927dd7a7d738545aae937c26
Jorge <valentinnave@gmail.com>
2024-12-02 13:35:52 -0300

    chiche

--- pages/ProfilePage/TabsSelectorMaterial.tsx
+++ pages/ProfilePage/TabsSelectorMaterial.tsx
@@ -3,13 +3,17 @@ import PersonalDataTab from './PersonalDataTab'
 import ChangePasswordTab from './ChangePasswordTab'
 import ProfilePage from './ProfilePage'
 import { NavigationContainer } from '@react-navigation/native'
+import { Colors } from '@/constants/Colors'
 
 const Tab = createMaterialTopTabNavigator()
 
 function TabsSelectorMaterial() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Foto">
+      <Tab.Navigator
+        initialRouteName="Foto"
+        screenOptions={{ tabBarPressColor: Colors.primary[200] }}
+      >
         <Tab.Screen name="Foto" component={ProfilePage} />
         <Tab.Screen name="Datos" component={PersonalDataTab} />
         <Tab.Screen name="Contraseña" component={ChangePasswordTab} />
