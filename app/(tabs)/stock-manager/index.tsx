440be9c2197e8ff902a03a048557fd85607cce7d
Jorge <valentinnave@gmail.com>
2024-12-02 10:08:35 -0300

    removed stock-manager from navigation

--- app/(tabs)/stock-manager/index.tsx
+++
@@ -1,7 +0,0 @@
-import { useAuthorizedUser } from '@/hooks/useUser'
-import StockManagerPage from '@/pages/StockPage/StockManagerPage'
-
-export default function () {
-  useAuthorizedUser()
-  return <StockManagerPage />
-}
--- app/(tabs)/stock-manager/stock-summary.tsx
+++
@@ -1,7 +0,0 @@
-import { useAuthorizedUser } from '@/hooks/useUser'
-import StockSummaryPage from '@/pages/StockPage/StockSummaryPage'
-
-export default function () {
-  useAuthorizedUser()
-  return <StockSummaryPage />
-}
